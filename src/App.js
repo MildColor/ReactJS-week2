7ca1800e7f8c89f9090ebd5bb50f006d5f870495
MildColor <96479626+MildColor@users.noreply.github.com>
2022-10-07 14:38:07 +0900

    dispatch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+// src/App.js
+
 import React from "react";
-import { useSelector } from "react-redux"; // import 해주세요.
+import { useDispatch } from "react-redux"; // import 해주세요.
 
 const App = () => {
-  const counterStore = useSelector((state) => state); // 추가해주세요.
-  console.log(counterStore); // 스토어를 조회해볼까요?
-
-  return <div></div>;
+  const dispatch = useDispatch(); // dispatch 생성
+  return (
+    <div>
+      <button
+        // 이벤트 핸들러 추가
+        onClick={() => {
+          // 마우스를 클릭했을 때 dispatch가 실행되고, ()안에 있는 액션객체가 리듀서로 전달된다.
+          dispatch({ type: "PLUS_ONE" });
+        }}
+      >
+        + 1
+      </button>
+    </div>
+  );
 };
 
 export default App;
